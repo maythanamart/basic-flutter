556551eefe89b2aa9b5b96300b022b8cfdb2e385
Thanamart <naiinz.may@gmail.com>
2022-06-08 17:18:47 +0700

    Keep secret key on correctly environment

--- django-webapi/flutterapi/flutterapi/settings.py
+++ django-webapi/flutterapi/flutterapi/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z-m=p2v=1c*l3*%f@#sje2#f_sr!@nn7fio5!m9iipjocext&v'
+import os
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
